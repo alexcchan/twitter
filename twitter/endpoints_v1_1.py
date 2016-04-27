98ed31aa995bfdf08b2b069c00ecc0d0b0b29b90
Alex Chan <alex@twilio.com>
2016-04-27 02:57:17 -0500

    Add method to show a status by id.

--- twitter/endpoints_v1_1.py
+++ twitter/endpoints_v1_1.py
@@ -6,9 +6,14 @@ mapping_table = {
 
     'content_type': 'application/json',
     'path_prefix': '/1.1',
+
     'search_tweets' : {
         'path': '/search/tweets.json',
-        'valid_params': ['q']
+        'valid_params': ['q'],
     },
 
+    'show_status' : {
+        'path': '/statuses/show.json',
+        'valid_params': ['id'],
+    },
 }
