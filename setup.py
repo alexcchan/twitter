0940dd67434e682b6be3e4c5b6ab5b0bb8b4b43a
Alex Chan <alex@twilio.com>
2016-07-28 02:29:00 -0500

    Added setup.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
 	packages = ['twitter'],
 	include_package_data = True,
 	install_requires = ['httplib2', 'simplejson'],
-	license='LICENSE.txt',
 	url = 'https://github.com/alexcchan/twitter/tree/master',
 	keywords = 'twitter api',
 	description = 'Twitter API Wrapper for Python',
